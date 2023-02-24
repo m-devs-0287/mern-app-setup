b068b0bbbc49496a784ebf0f23137138aa12365d
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-23 22:21:53 -0600

    Update App.jsx
    adding Navbar.jsx + react router
    adding Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-
-import './styles/App.css'
+import { Routes, Route } from "react-router-dom";
+import "./styles/App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>MERN APP SETUP</h1>
+      <Navbar>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Navbar>
+      <main>
+        <Home/>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

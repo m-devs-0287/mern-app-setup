6d31bca639826d3798dd2561fa7da2f98124a94c
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-24 23:34:35 -0600

    Update WorkoutForm.jsx

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -4,9 +4,37 @@ const WorkoutForm = () => {
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const workout = { title, load, reps };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      setTitle("");
+      setLoad("");
+      setReps("");
+      setError(null);
+      console.log("new workout added");
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>Add a new workout</h3>
 
       <label>Exercice Title</label>
@@ -19,18 +47,19 @@ const WorkoutForm = () => {
       <label>Load (in kg):</label>
       <input
         type="number"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
       />
 
       <label>Reps: </label>
       <input
         type="number"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
       />
 
       <button>Add Workout</button>
+      {error && <div>{error}</div>}
     </form>
   );
 };

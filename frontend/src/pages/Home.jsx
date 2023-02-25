5c98172f84ec6413e8d743774d3b80b25ad97f4c
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-02-24 23:35:23 -0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import WorkoutDetails from "../components/workoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -18,11 +19,12 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {workouts &&
-        workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout} />
-        ))}
+    <div className="home">
+      <WorkoutForm />
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
     </div>
   );
 };
